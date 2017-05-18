fd9e9947b2e2aafaa8f3d33ff0c275a5c5a0d88e
dart-wakar <wakar@whitelogic.in>
2017-05-19 02:33:38 +0530

    Fixed Main navigation back press and enabled double back press to exit

--- app/src/main/java/com/wakarkhan/deliverydrop/NavigationMain.java
+++ app/src/main/java/com/wakarkhan/deliverydrop/NavigationMain.java
@@ -2,7 +2,6 @@ package com.wakarkhan.deliverydrop;
 
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
